f40548469cd089fb6001f810e0a674ce5877e659
kerem.duzenli <krmdznl97@gmail.com>
2023-06-27 13:10:09 -0300

    2023.06.27

---
+++ 0_Main/Program.cs
@@ -0,0 +1,24 @@
+﻿using _1_TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] liste = solution.TwoSum(new int[] { 3, 2, 4 }, 6);
+            string result = "[" + string.Join(", ", liste) + "]";
+            Console.WriteLine(result);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
